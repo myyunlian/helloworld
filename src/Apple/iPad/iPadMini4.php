d73364b7246c519dfb49883060562aa02bfd1e44
陈立刚 <chenlg@mysoft.com.cn>
2017-11-14 19:08:50 +0800

    init

---
+++ src/Apple/iPad/iPadMini4.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Apple\iPad;
+
+class iPadMini4
+{
+    public static function info()
+    {
+        echo "This is Apple iPad mini 4!<br/>";
+    }
+}
+
---
+++ src/Apple/iPhone/iPhoneX.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Apple\iPhone;
+
+class iPhoneX
+{
+    public static function info()
+    {
+        echo "This is Apple iPhoneX!<br/>";
+    }
+}
+
